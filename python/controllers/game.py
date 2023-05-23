5d6dc199e9d242a8264087c3ed74502f74216515
Sinsinful <sin_1210@hotmail.com>
2023-05-23 23:36:04 +0100

    Basic functions complete

--- python/controllers/game.py
+++ python/controllers/game.py
@@ -14,15 +14,28 @@ def generate_word():
     return random.choice(word_list)
 
 def mask_word(word, guessed_letters):
+    #lower case the word for guesses to match
     masked_word = ""
+    word = word.lower()
     for letter in word:
-            masked_word += "_"
+            if letter not in guessed_letters:
+                masked_word += "_"
+            elif letter in guessed_letters:
+                masked_word += letter
     return masked_word.strip()
 
 def is_valid_guess(guess, game):
     if not guess.isalpha() or len(guess) != 1:
         return False
     return True
+
+def remove_letters(a, b):
+    c = []
+    for letter in a:
+        if letter not in b:
+            c.append(letter)
+    return c
+
 
 @mod.route('/', methods=['POST'])
 def start_game():
@@ -44,7 +57,7 @@ def get_game_state(game_id):
     return jsonify({
         "incorrect_guesses": game["guessed_letters"],
         "remaining_attempts": game["attempts"],
-        "status": "In Progress",
+        "status": game["status"],
         "word": masked_word,
     })
 
@@ -59,11 +72,61 @@ def make_guess(game_id):
     if not is_valid_guess(guess, game):
         return jsonify({"Message": "Guess must be supplied with 1 letter"}), 400
     
+    #return error message if letter already used
+    if guess in game["guessed_letters"]:
+        return jsonify({"Message": "Letter already guessed. Please choose another letter."}), 400
+    
+    #check for game lost status
+    if game["attempts"] == 0:
+        game["status"] = "Lost"
+        return jsonify({
+        "game_over": "Game over no further guesses can be made",
+        "incorrect_guesses": game["guessed_letters"],
+        "remaining_attempts": game["attempts"],
+        "status": game["status"],
+        "game word was": game["word"],
+    })
+
+
+   
+    #subtract an atttempt if guessed letter not in word
+    if guess not in game["word"]:      
+        game["attempts"] = game["attempts"] -1
+    
+    #game loss check 
+    if game["attempts"] < 1:
+        game["status"] = "Lost"
+        return jsonify({
+        "incorrect_guesses": game["guessed_letters"],
+        "remaining_attempts": game["attempts"],
+        "status": game["status"],
+        "game word was": game["word"],
+    })
+
+    #add guessed letter to guessed list
+    game["guessed_letters"] += guess
+             
     masked_word = mask_word(game["word"], game["guessed_letters"])
+    masked_word = masked_word.lower()
+    game["word"] = game["word"].lower()
+
+    #check for game win
+    if game["word"] == masked_word:
+        game["status"] = "Won"
+        return jsonify({
+        "winner": "Congratulations you have won.",
+        "incorrect_guesses": remove_letters(game["guessed_letters"], game["word"]),
+        "remaining_attempts": game["attempts"],
+        "status": game["status"],
+        "unmasked": game["word"],
+    })
+
+
 
     return jsonify({
-        "incorrect_guesses": game["guessed_letters"],
+        "incorrect_guesses": remove_letters(game["guessed_letters"], game["word"]),
         "remaining_attempts": game["attempts"],
         "status": "In Progress",
         "word": masked_word,
+        "unmasked": game["word"],
     })
